12e3585b20419280b640083bbf6cbf8bd6b52f3e
Ved Vikas <ved.vikas@sunplussoftware.com>
2025-05-20 18:02:28 +0530

    added login and logout functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,49 @@
 import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { logout, reset } from "../features/auth/authSlice";
 
 
 const Header = () => {
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const { user } = useSelector(state => state.auth)
+
+
+    const logoutFn = () => {
+        dispatch(logout())
+        dispatch(reset())
+        navigate('/login')
+    }
+
     return (
         <header className='header'>
             <div className='logo'>
                 <Link to='/'>Task Creator</Link>
             </div>
             <ul>
-                <li>
-                    <Link to='/login'>
-                        <FaSignInAlt /> Login
-                    </Link>
-                </li>
+                {user ? (
+                    <li>
+                        <button className='btn' onClick={logoutFn}>
+                            <FaSignOutAlt /> Logout
+                        </button>
+                    </li>
+                ) : (
+                    <>
+                        <li>
+                            <Link to='/register'>
+                                <FaUser /> Register
+                            </Link>
+                        </li>
 
-                <li>
-                    <Link to='/register'>
-                        <FaUser /> Register
-                    </Link>
-                </li>
-
-                <li>
-                    <Link to='/logout'>
-                        <FaSignOutAlt /> Logout
-                    </Link>
-                </li>
-
+                        <li>
+                            <Link to='/login'>
+                                <FaSignInAlt /> Login
+                            </Link>
+                        </li>
+                    </>
+                )}
             </ul>
         </header>
     );
